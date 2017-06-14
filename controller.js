bb9cd72f493c14e19a873a26824ccc6853ea77bc
Jacob Hamblin <jacobhamblin83@gmail.com>
2017-06-14 17:19:23 -0600

    updated portfolio

--- controller.js
+++ controller.js
@@ -1,6 +1,4 @@
 angular.module('app').controller('controller', function($scope, service) {
-    $scope.test = 'hello';
-    $scope.servicetest = service.test;
 
     $scope.send = function(newObj){
         service.send(newObj)
